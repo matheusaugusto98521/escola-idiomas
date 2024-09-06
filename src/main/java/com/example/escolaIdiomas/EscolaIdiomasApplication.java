bcc53fcc7f577b38ce2708892c88525cefd4b361
aluno <aluno@fateclins.local>
2024-09-05 21:56:28 -0300

    first

---
+++ src/main/java/com/example/escolaIdiomas/EscolaIdiomasApplication.java
@@ -0,0 +1,13 @@
+package com.example.escolaIdiomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaIdiomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaIdiomasApplication.class, args);
+	}
+
+}
